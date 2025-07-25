a1ef90a564a66587808c2eb262144f5ca421288f
DanteSolorzano <solorzanodante19@gmail.com>
2025-07-24 18:41:45 -0600

    feat: all the crud actions are complete, I implement devTools of zustand to watch the state

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,11 +1,19 @@
 import type { Patient } from "../types"
 import PatientDetailItem from "./PatientDetailItem"
+import { usePatientStore } from "../store"
 
 type PatientDetailsProps = {
     patient: Patient
 }
 
 export default function PatientDetails({patient} : PatientDetailsProps) {
+
+   const deletePatient = usePatientStore((state) => state.deletePatient)
+   const getPatientById = usePatientStore((state) => state.getPatientById)
+
+   
+
+
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
 
@@ -15,12 +23,20 @@ export default function PatientDetails({patient} : PatientDetailsProps) {
       <PatientDetailItem label="Admition date" data={patient.date.toString()} />
       <PatientDetailItem label="symptoms" data={patient.symptoms} />
 
-      <div className="flex justify-between gap-2 mt-10">
-        <button className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg">
+      <div className="flex flex-col lg:flex-row gap-3 justify-between mt-10">
+        <button 
+        type="button"
+        className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+        onClick={() => getPatientById(patient.id)}
+        >
         Edit Pet
         </button>
 
-        <button className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg">
+        <button 
+        type="button"
+        className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+        onClick={() => deletePatient(patient.id)}
+        >
         Delete Pet
         </button>
       </div>
